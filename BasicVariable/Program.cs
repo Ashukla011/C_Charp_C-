f0a35e32a8269139d6abdb96de607bd3e5135d30
Ashukla011 <shuklavashu9696@gmail.com>
2024-09-28 19:57:08 +0530

    cs

---
+++ BasicVariable/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a =10,b=10;
+int sum = a+b;
+Console.WriteLine("sum of a + b:{0}",sum);
+
+int num1,num2;
+Console.WriteLine("Enter First Number");
+num1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Secound Number");
+num2=Convert.ToInt32(Console.ReadLine());
+
+int sum1 = num1+num2;
+Console.WriteLine("sun1:{0}",sum1);
---
+++ ConditinalStatement/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+//  Convert.toChar -> used to convert int to str
+// Conditional and switch statement 
+
+char ch;
+Console.WriteLine("Enter The Day Name:");
+ch =Convert.ToChar(Console.ReadLine());
+ switch(ch)
+ {
+    case 'a':
+    Console.WriteLine("Monday");
+    break;
+
+    case 'b':
+    Console.WriteLine("Tuesday");
+    break;
+
+    case 'c':
+    Console.WriteLine("Wensday");
+    break;
+    
+    case 'd':
+    Console.WriteLine("Thursday");
+    break;
+
+    case 'f':
+    Console.WriteLine("Friday");
+    break;
+
+    case 'g':
+    Console.WriteLine("Suterday");
+    break;
+
+    case 'h':
+    Console.WriteLine("Sunday");
+    break;
+
+    default :
+    Console.WriteLine("Sorrynotmatching");
+    break;
+
+ }
+
+//  Secound case :
+char ch;
+Console.WriteLine("Enter The Day Name:");
+ch =Convert.ToChar(Console.ReadLine());
+ switch(ch)
+ {
+    case 'a':
+    case 'u':  
+    case 'i':   
+    case 'o':    
+    case 'e':
+     Console.WriteLine("Vowel");
+     break;
+
+    default :
+    Console.WriteLine("it is consonent");
+    break;
+
+ }
